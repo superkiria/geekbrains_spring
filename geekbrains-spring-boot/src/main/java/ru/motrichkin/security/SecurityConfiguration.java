ec6f3250b2227040e46c4b1c2a74e7ee1e8221fe
Kirill Mot <kirill.motrichkin@ya.ru>
2021-02-07 22:13:06 +0300

    start of security

---
+++ geekbrains-spring-boot/src/main/java/ru/motrichkin/security/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package ru.motrichkin.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    public void authConfigure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}password")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("ADMIN");
+    }
+
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**")
+                    .authorizeRequests()
+                    .anyRequest()
+                    .hasRole("ADMIN")
+                    .and()
+                    .httpBasic();
+        }
+
+    }
+
+    @Configuration
+    @Order(2)
+    public static class UiWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .formLogin();
+        }
+
+    }
+
+}
