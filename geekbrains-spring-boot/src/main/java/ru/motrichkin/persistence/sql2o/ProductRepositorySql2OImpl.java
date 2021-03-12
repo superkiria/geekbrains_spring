0f634ae2abaa55651ec794e89a1de4b1907b139f
Kirill Mot <kirill.motrichkin@ya.ru>
2021-03-12 22:26:58 +0300

    temp sql2o start

--- geekbrains-spring-boot/src/main/java/ru/motrichkin/persistence/sql2o/ProductRepositorySql2OImpl.java
+++ geekbrains-spring-boot/src/main/java/ru/motrichkin/persistence/sql2o/ProductRepositorySql2OImpl.java
@@ -3,6 +3,7 @@ package ru.motrichkin.persistence.sql2o;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
@@ -10,20 +11,22 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import ru.motrichkin.persistence.Product;
 import ru.motrichkin.persistence.interfaces.ProductRepository;
-import ru.motrichkin.service.ProductService;
 
 import java.util.List;
 import java.util.Optional;
 
+import static java.lang.Math.min;
+
 @Primary
 @Component
-public class ProductRepositorySql20Impl implements ProductRepository {
+public class ProductRepositorySql2OImpl implements ProductRepository {
 
     private final Sql2o sql2o;
 
     private static final String SELECT_PRODUCTS_QUERY = "select id, title, cost, from product";
+    private static final String SELECT_ONE_PRODUCT_QUERY = "select id, title, cost, from product where id = :productId";
 
-    public ProductRepositorySql20Impl(@Autowired Sql2o sql2o) {
+    public ProductRepositorySql2OImpl(@Autowired Sql2o sql2o) {
         this.sql2o = sql2o;
     }
 
@@ -34,7 +37,13 @@ public class ProductRepositorySql20Impl implements ProductRepository {
 
     @Override
     public Optional<Product> findById(Long id) {
-        return Optional.empty();
+        try (Connection connection = sql2o.open()) {
+            return connection.createQuery(SELECT_ONE_PRODUCT_QUERY, false)
+                    .addParameter("productId", id)
+                    .setColumnMappings(Product.COLUMN_MAPPINGS)
+                    .executeAndFetch(Product.class)
+                    .stream().findAny();
+        }
     }
 
     public List<Product> findAll() {
@@ -47,13 +56,21 @@ public class ProductRepositorySql20Impl implements ProductRepository {
 
     @Override
     public Page<Product> findAll(Pageable pageable) {
-        return null;
+        List<Product> products = findAll();
+        return new PageImpl<>(
+                products.subList((int) pageable.getOffset(), (int) pageable.getOffset() + pageable.getPageSize()),
+                pageable,
+                products.size());
     }
 
     @Override
     public Page<Product> findAll(Specification<Product> productSpecification, Pageable pageable) {
-        return null;
+        List<Product> products = findAll();
+        return new PageImpl<>(
+                products.subList((int) pageable.getOffset(),
+                                    min((int) pageable.getOffset() + pageable.getPageSize(), products.size())),
+                pageable,
+                products.size());
     }
 
-    ;
 }
